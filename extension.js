a892aa323ed04c8a64ccabe6eb2313d94dfbea6c
Bestony <bestony@linux.com>
2023-04-15 16:22:19 +0800

    feat: done full page review

--- extension.js
+++ extension.js
@@ -1,14 +1,15 @@
+const { Range } = require('vscode');
 const vscode = require('vscode');
 const axios = require("axios").default;
 function activate(context) {
-	let reviewSelection = vscode.commands.registerCommand('cofinder.reviewSeletion', function () {
+	/**
+	 * 服务端 Review 函数（带进度条）
+	 * @param {string} code 需要进行 Review 的 Code
+	 * @param {Range} selection 具体的范围，如果不传递，则为全文 Review
+	 */
+	function ReviewCodeByAI(code,selection = null){
 		const editor = vscode.window.activeTextEditor;
-		if (!editor) {
-			vscode.window.showInformationMessage("识别失败，请联系开发者排查");
-		}
 		const document = editor.document;
-		const selection = editor.selection;
-		const code = document.getText(selection);
 
 		vscode.window.withProgress(
 			{
@@ -23,7 +24,12 @@ function activate(context) {
 				});
 				if(data.code == 0){
 					editor.edit(editBuilder => {
-						editBuilder.replace(selection, `// ${data.data} \r\n\r\n` + code);
+						if(selection == null){
+							let fullPageRange = new Range(document.lineAt(0).range.start, document.lineAt(document.lineCount - 1).range.end)
+							editBuilder.replace(fullPageRange, `// ${data.data} \r\n\r\n` + code)
+						}else{
+							editBuilder.replace(selection, `// ${data.data} \r\n\r\n` + code);
+						}
 					});
 					progress.report({ increment: 100, message: "" })
 					vscode.window.showInformationMessage("🤖 AI 生成成功，请查看代码前的注释内容。")
@@ -32,6 +38,17 @@ function activate(context) {
 				}			
 			}
 		)
+	}
+	let reviewSelection = vscode.commands.registerCommand('cofinder.reviewSeletion', function () {
+		const editor = vscode.window.activeTextEditor;
+		if (!editor) {
+			vscode.window.showInformationMessage("识别失败，请联系开发者排查");
+		}
+		const document = editor.document;
+		const selection = editor.selection;
+		const code = document.getText(selection);
+		ReviewCodeByAI(code,selection);
+		
 	});
 	let reviewFile = vscode.commands.registerCommand("cofinder.reviewFile",function(){
 		const editor = vscode.window.activeTextEditor;
@@ -39,9 +56,8 @@ function activate(context) {
 			vscode.window.showInformationMessage("识别失败，请联系开发者排查");
 		}
 		const document = editor.document;
-		console.log(document.languageId);
-		console.log(vscode.languages.getLanguages());
-		vscode.window.showInformationMessage("🤖 AI 生成成功，请查看代码前的注释内容。")
+		const code = document.getText();
+		ReviewCodeByAI(code);
 	})
 	context.subscriptions.push(reviewSelection);
 	context.subscriptions.push(reviewFile);
