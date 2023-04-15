231eea549e92df6c72179e65fd24c6bdfadb5192
Bestony <bestony@linux.com>
2023-04-16 00:50:17 +0800

    feat: remove unused code

--- request.js
+++ request.js
@@ -36,7 +36,7 @@ async function local(action, template, code, selection = null, progress) {
         apiKey: localMode.key,
     });
     const openai = new OpenAIApi(configuration);
-    if(localMode.model == "text-davinci-003"){
+    if (localMode.model == "text-davinci-003") {
         const { status, data } = await openai.createCompletion({
             model: localMode.model,
             prompt: templateString.replace("{code}", code),
@@ -54,18 +54,17 @@ async function local(action, template, code, selection = null, progress) {
                 progress.report({ increment: 100, message: "" })
                 vscode.window.showInformationMessage("🤖 AI 理解成功，请查看代码前的 AI 为你写的建议。")
             });
-    
+
         } else {
             vscode.window.showInformationMessage("🤖 AI 理解失败，请联系开发者排查")
         }
-    }else{
-        const {data,status} = await openai.createChatCompletion({
+    } else {
+        const { data, status } = await openai.createChatCompletion({
             model: localMode.model,
-            messages: [{role: "system", content: templateString.replace("{code}", code)}],
-          });
+            messages: [{ role: "system", content: templateString.replace("{code}", code) }],
+        });
 
         if (status == 200) {
-            console.log(data.choices,data.choices[0].message)
             editor.edit(editBuilder => {
                 if (selection == null) {
                     let fullPageRange = new Range(document.lineAt(0).range.start, document.lineAt(document.lineCount - 1).range.end)
@@ -76,13 +75,11 @@ async function local(action, template, code, selection = null, progress) {
                 progress.report({ increment: 100, message: "" })
                 vscode.window.showInformationMessage("🤖 AI 理解成功，请查看代码前的 AI 为你写的建议。")
             });
-    
+
         } else {
             vscode.window.showInformationMessage("🤖 AI 理解失败，请联系开发者排查")
         }
     }
-    
-
 }
 module.exports = {
     cloud,
