8c27fb17ea7c299ddf669fa83cfc98537716b066
Bestony <bestony@linux.com>
2023-04-15 14:31:36 +0800

    feat: add server api

--- extension.js
+++ extension.js
@@ -1,20 +1,35 @@
 const vscode = require('vscode');
+const axios = require("axios").default;
 function activate(context) {
 	let reviewSelection = vscode.commands.registerCommand('cofinder.reviewSeletion', function () {
 		const editor = vscode.window.activeTextEditor;
-		if (editor) {
-			const document = editor.document;
-			const selection = editor.selection;
-
-			const code = document.getText(selection);
-			editor.edit(editBuilder => {
-				editBuilder.replace(selection,"// Hello World \r\n"  + code);
-			});
-		
-		} else {
-			console.log("failed get editor")
+		if (!editor) {
+			vscode.window.showInformationMessage("识别失败，请联系开发者排查");
 		}
-		vscode.window.showInformationMessage('Review Whole File');
+		const document = editor.document;
+		const selection = editor.selection;
+		const code = document.getText(selection);
+
+		vscode.window.withProgress(
+			{
+				location: vscode.ProgressLocation.Notification,
+				title: 'Finding ...',
+				cancellable: false,
+			},
+			async (progress, token) => {
+				let { server } = vscode.workspace.getConfiguration('cofinder');
+				const {data} = await axios.get(`${server}/review`);
+				if(data.code == 0){
+					editor.edit(editBuilder => {
+						editBuilder.replace(selection, `// ${data.data} \r\n\r\n` + code);
+					});
+					progress.report({ increment: 100, message: "" })
+					vscode.window.showInformationMessage("AI 生成成功，请查看代码前的注释内容。")
+				}else{
+					vscode.window.showInformationMessage("AI 生成失败，请联系开发者排查")
+				}			
+			}
+		)
 	});
 	context.subscriptions.push(reviewSelection);
 }
